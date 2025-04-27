7819801a36df60c66e3ebd4a7891e3c23c58226c
vozar.david <vozar.david8@gmail.com>
2025-04-27 16:23:59 +0200

    feat-add variant prop and refactor classes, use classNames lib

--- client/src/components/BackButton.jsx
+++ client/src/components/BackButton.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import classNames from 'classnames'
+import { useNavigate } from 'react-router-dom'
 import BackArrow from '../assets/icons/arrow_left.svg'
+import './BackButton.scss'
 
-function BackButton() {
+function BackButton({
+  variant = 'default'
+}) {
 
     const navigate = useNavigate();
 
   return (
-    <button onClick={() => navigate(-1)} className='back-btn'>
-      <img src={BackArrow} alt="back" />
+    <button onClick={() => navigate(-1)} className={classNames(
+      'back-btn', 
+      `back-btn--${variant}`,
+    )}>
+      <img src={BackArrow} alt="Back icon" />
       <span className='back-text'>Go back</span>
     </button>
   )
