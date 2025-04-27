b08f29047077f45395ccd3ab79ec310f54dc1152
vozar.david <vozar.david8@gmail.com>
2025-04-27 16:37:55 +0200

    refactor: update syntax, use NavLink

--- client/src/components/Navigation.jsx
+++ client/src/components/Navigation.jsx
@@ -1,17 +1,44 @@
-import React from 'react'
-import './Navigation.scss'
+// src/components/Navigation.jsx
 
-function Navigation() {
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+import classNames from 'classnames';
+import './Navigation.scss';
+
+function Navigation({ variant = 'default'}) {
+  
+  const links = [
+    { to: '/productlist', label: 'Home' },
+    { to: '/contact', label: 'Contact' },
+    { to: '/about', label: 'About' }
+  ]
+
   return (
-    <div className='navigation'>
-        <ul>
-            <li>Marketplace</li>
-            <li>About</li>
-            <li>Contact</li>
-            <li>Profile</li>
-        </ul>
-    </div>
-  )
+    <nav
+      className={classNames(
+        'navigation',
+        `navigation--${variant}`,
+      )}
+      aria-label="Main navigation"
+    >
+      <ul className="navigation__list">
+        {links.map(({ to, label }) => (
+          <li key={to} className="navigation__item">
+            <NavLink
+              to={to}
+              className={({ isActive }) =>
+                classNames('navigation__link', {
+                  'navigation__link--active': isActive,
+                })
+              }
+            >
+              {label}
+            </NavLink>
+          </li>
+        ))}
+      </ul>
+    </nav>
+  );
 }
 
-export default Navigation
+export default Navigation;
