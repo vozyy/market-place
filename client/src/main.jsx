a468793c4455d7a34a378593fa56cd1fb8833c7e
vozar.david <vozar.david8@gmail.com>
2025-05-22 21:47:02 +0200

    refactor: remove redundant context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,12 +3,9 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import 'react-loading-skeleton/dist/skeleton.css'
 import App from './App.jsx'
-import { ListContext } from './features/list/context/listContext.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <ListContext.Provider value={'hodnota z contextu'}>
       <App />
-    </ListContext.Provider>
   </StrictMode>,
 )
