53e9a1c458b25ec001bfb71cc02a06a3539e99d3
vozar.david <vozar.david8@gmail.com>
2025-05-22 23:03:02 +0200

    feat: use listprovider

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,9 +3,12 @@ import { createRoot } from 'react-dom/client'
 import './styles/index.scss'
 import 'react-loading-skeleton/dist/skeleton.css'
 import App from './App.jsx'
+import { ListProvider } from './features/list/provider/ListProvider.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-      <App />
+      <ListProvider>
+        <App />
+      </ListProvider>
   </StrictMode>,
 )
