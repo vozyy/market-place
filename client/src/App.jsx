a9ce32a478a1d7839c0a3b3388ab31a9e68bb2be
vozar.david <vozar.david8@gmail.com>
2025-05-27 19:38:20 +0200

    feat: add new route /list

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,6 +5,7 @@ import PageSkeleton from '@shared/components/PageSkeleton'
 const HomePage = lazy(() => import('@pages/Homepage'));
 const Productlist = lazy(() => import('@pages/Productlist'));
 const ProductDetail = lazy(() => import('@pages/ProductDetail'));
+const ListPage = lazy(() => import('@pages/ListPage'));
 
 function App() {
   
@@ -15,6 +16,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/productlist" element={<Productlist />} />
           <Route path="/product/:id" element={<ProductDetail />} />
+          <Route path="/list" element={<ListPage />} />
         </Routes>
       </Suspense>
     </BrowserRouter>
